236112dcfa93fd14eebf131093db16b75bf365df
hackster07 <beylava101@gmail.com>
2024-12-06 12:55:22 -0800

    PID tuning works and ready to go

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/stuff/PIDTuning/PIDTesting.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode.common.stuff.PIDTuning;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+@Config
+@TeleOp
+public class PIDTesting extends LinearOpMode {
+    public static double p = 0.02;
+    public static double i = 0;
+    public static double d = 0.0001;
+    public static int target = 0;
+
+    public void runOpMode() {
+        DcMotor frontLeft = hardwareMap.dcMotor.get("leftFront");
+        DcMotor backLeft = hardwareMap.dcMotor.get("leftBack");
+        DcMotor frontRight = hardwareMap.dcMotor.get("rightFront");
+        DcMotor backRight = hardwareMap.dcMotor.get("rightBack");
+        Servo leftLinkage = hardwareMap.get(Servo.class, "leftLinkage");
+        Servo rightLinkage = hardwareMap.get(Servo.class, "rightLinkage");
+        Servo bottomWrist = hardwareMap.get(Servo.class, "bottomWrist");
+
+        //Currently the one I care about
+        DcMotorEx slideMotor = hardwareMap.get(DcMotorEx.class, "slideMotor");
+        slideMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        //Slides look good at 0.02, 0, 0.0001
+
+        PIDController controller = new PIDController(p, i, d);
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+            controller.setPID(p, i, d);
+            int slidePos = slideMotor.getCurrentPosition();
+            double pid = controller.calculate(slidePos, target);
+
+            slideMotor.setPower(pid * .8);
+
+            telemetry.addData("CurrPos: ", slidePos);
+            telemetry.addData("Target: ", target);
+            telemetry.update();
+        }
+    }
+}
